29a264ccaf13a36a75fe6216171cfc70f4afdd3f
Robert Sorkin <robasorkin@gmail.com>
2016-08-29 14:14:48 +0300

    add sequlize demo

---
+++ server.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+	res.send('Todo API Root');
+});
+
+app.get('/todos', function(req, res) {
+	var queryParamas = req.query;
+	var filteredTodos = todos;
+
+	if (queryParamas.hasOwnProperty('completed') && queryParamas.completed == 'true') {
+		filteredTodos = _.where(filteredTodos, {
+			completed: true
+		});
+	} else if (queryParamas.hasOwnProperty('completed') && queryParamas.completed == 'false') {
+		filteredTodos = _.where(filteredTodos, {
+			completed: false
+		});
+	}
+	if (queryParamas.hasOwnProperty('q') && queryParamas.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParamas.q.toLowerCase()) > -1;
+		});
+	}
+
+
+	res.json(filteredTodos);
+});
+
+app.get('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
+
+	if (matchedTodo) {
+		res.send(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+});
+
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
+	if (!matchedTodo) {
+		res.status(404).json({
+			"error": "no todo found with that id"
+		});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
+app.put('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	matchedTodo = _.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
+
+
+app.post('/todos', function(req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
+});
+
+app.listen(PORT, function(req, res) {
+	console.log('Express listening on port ' + PORT + '!');
+});
